d3b09c0f5b00349c386a33419fda0cabe11ed9ff
Aravind Cheekkallur <aravind@pointcross.net>
2019-09-04 15:24:01 +0530

    Updates

---
+++ precache-manifest.d0924d95e38ad63bb4ad753a768f3363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644e859c2adfa717da8d0adbdd15c72d",
+    "url": "/ERail-React-App/index.html"
+  },
+  {
+    "revision": "7c84dee1b12bb7722d39",
+    "url": "/ERail-React-App/static/css/main.55e7120c.chunk.css"
+  },
+  {
+    "revision": "0fd741931e0d14c8e013",
+    "url": "/ERail-React-App/static/js/2.d747f9bc.chunk.js"
+  },
+  {
+    "revision": "7c84dee1b12bb7722d39",
+    "url": "/ERail-React-App/static/js/main.fca04c3b.chunk.js"
+  },
+  {
+    "revision": "565c068fc440ac41c5f1",
+    "url": "/ERail-React-App/static/js/runtime~main.63e80bd3.js"
+  }
+]);
